1dd90d95e4e8c36426b206942498fbda5535eb0e
jmccut <jmccut@essex.ac.uk>
2018-01-02 21:47:46 +0000

    Input for second scene
    Added:
    -Joystick and jump button
    -Support for cross-platform input controls
    -Updated player collision to work with new controls

---
+++ Final Year Project/Assets/Scripts/CharController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(vertical, 0f, -horizontal);
+    }
+}
